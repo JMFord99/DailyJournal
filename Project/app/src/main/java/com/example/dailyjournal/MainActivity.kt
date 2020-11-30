27efb7950a19baeb5f7f852bad114f564302a64b
Lizi <lizi@jordanz.net>
2020-11-30 16:24:22 -0500

    Added button to calendar view

--- Project/app/src/main/java/com/example/dailyjournal/MainActivity.kt
+++ Project/app/src/main/java/com/example/dailyjournal/MainActivity.kt
@@ -1,11 +1,38 @@
 package com.example.dailyjournal
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.CalendarView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.top_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.past_entries -> {
+                val intent = Intent(this, com.example.dailyjournal.CalenderView::class.java);
+                startActivity(intent);
+                true;
+            }
+            else -> false
+        }
+    }
+
+
+
+
 }
+
